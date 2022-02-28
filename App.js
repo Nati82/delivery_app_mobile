8125d5089383d30a6f2b7a9093386eca71321523
Nati82 <ashenafinati82@gmail.com>
2022-02-28 14:14:16 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, StatusBar, Text, View, SafeAreaView } from "react-native";
+
+export default function App() {
+  return (
+    <>
+      <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
+        <View style={{ padding: 16, backgroundColor: "green" }}>
+          <Text>search</Text>
+        </View>
+        <View style={{ flex: 1, padding: 16, backgroundColor: "blue" }}>
+          <Text>list</Text>
+        </View>
+      </SafeAreaView>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({});
